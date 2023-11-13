1036d2de4c516561b9df1a65b4c6417a2e25e3ea
Mahmoud Ahmed <126110594+Mahmoud921@users.noreply.github.com>
2023-11-13 23:01:49 +0200

    Classes and Objects

---
+++ Test OOP/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    enum Fuel
+    {
+        Gasoline,
+        Diesel,
+        Electric,
+        Hybrid,
+    }
+    class Car
+    {
+        public string CarBrand { get; set; }
+        public string CarModel {  get; set; }
+        public string CarColor {get; set; }
+        public int MakeYear { get; set; }
+        public Fuel Fuel { get; set; } 
+        public Car(string carBrand, string carModel, string carColor,int makeYear,Fuel fuel)
+        {
+            CarModel = carModel;
+            CarBrand = carBrand;
+            CarColor = carColor;
+            MakeYear = makeYear;
+            Fuel = fuel;
+        }
+
+    }
+}
---
+++ Test OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOP;
+
+class Program {
+    static void Main()
+    {
+        Console.WriteLine("=============== Car ===============");
+        Car car = new Car("BMW", "Xs6","Red",2022,Fuel.Electric);
+        Console.WriteLine($"Brand: {car.CarBrand}");
+        Console.WriteLine($"Model: {car.CarModel}");
+        Console.WriteLine($"Color: {car.CarColor}");
+        Console.WriteLine($"Year: {car.MakeYear}");
+        Console.WriteLine($"Fuel Type: {car.Fuel}");
+
+    }
+}
